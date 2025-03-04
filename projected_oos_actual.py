416fe3af0b65734825635de240c34ae5480b9f20
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-04 15:10:55 +0700

    Update projected_oos_actual.py

--- projected_oos_actual.py
+++ projected_oos_actual.py
@@ -186,7 +186,7 @@ if supply_file and oos_file:
     }
     
     # Create DataFrame
-    assump = pd.DataFrame(assume, index=False)
+    assump = pd.DataFrame(assume)
 
     with st.expander("View Assumptions"):
         # Apply styling to left-align text and reduce font size
