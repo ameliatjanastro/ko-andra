a0c03acb8a5bee1b2ca5ff43eee16a72c488f932
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-07 09:06:58 +0700

    Update projected_oos_actual.py

--- projected_oos_actual.py
+++ projected_oos_actual.py
@@ -91,13 +91,13 @@ if supply_file and oos_file:
         if date in fixed_oos_data["Date Key"].values:
             projected_oos = fixed_oos_data.loc[fixed_oos_data["Date Key"] == date, "OOS%"].values[0]
             supply = supply_data.loc[supply_data["Date"] == date]
-        elif date >= pd.to_datetime("2025-03-05") and date <= pd.to_datetime("2025-03-31"):
+        elif date >= pd.to_datetime("2025-03-05") and date <= pd.to_datetime("2025-03-09"):
             supply = extended_supply.loc[extended_supply["Date"] == date]
             # Apply L7 trend to estimate OOS%
-            prev_date = date - pd.Timedelta(days=1)
+            prev_date = date - pd.Timedelta(days=2)
             prev_oos_values = [entry["Projected OOS%"] for entry in oos_data if entry["Date"] == prev_date.strftime("%d %b %Y")]
             if prev_oos_values:
-                projected_oos = prev_oos_values[0] * (1 + avg_oos_increase)*1.5  # Apply trend
+                projected_oos = prev_oos_values[0] * (1 + avg_oos_increase)  # Apply trend
             else:
                 projected_oos = last_7_days_oos["OOS%"].mean()  # Use L7 avg if no previous OOS
         elif date < change_date:
@@ -131,7 +131,7 @@ if supply_file and oos_file:
 
     # Get Projected OOS for March 8
     oos_values = [entry["Projected OOS%"] for entry in oos_data if pd.to_datetime(entry["Date"]) in pd.date_range("2025-03-04", "2025-03-07") and not pd.isna(entry["Projected OOS%"])]
-    projected_oos_8mar = np.mean(oos_values)*0.7 if oos_values else 7  # Default to 12 if no valid values
+    projected_oos_8mar = np.mean(oos_values)*0.65 if oos_values else 7  # Default to 12 if no valid values
 
     # Adjust projection for March 9 onwards
     for entry in oos_data:
