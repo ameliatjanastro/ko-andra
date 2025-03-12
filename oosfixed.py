b76f48a4f7a8ff3c889523b5258264ccb9b08e25
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-12 20:58:07 +0700

    Update oosfixed.py

--- oosfixed.py
+++ oosfixed.py
@@ -36,7 +36,7 @@ if oos_wh_file:
     oos_wh_data["Date"] = pd.to_datetime(oos_wh_data["Date"])  # Convert OOS WH dates
 
     # OOS Projection
-    target_dates = pd.date_range(start=pd.to_datetime("2025-03-13"), periods=21, freq='D')
+    target_dates = pd.date_range(start=pd.to_datetime("2025-03-13"), periods=26, freq='D')
     oos_data = []
 
     for date in target_dates:
