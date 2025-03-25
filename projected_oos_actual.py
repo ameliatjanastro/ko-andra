0384493d125cd909df675a5230f45135b88c0b16
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-25 12:45:00 +0700

    Update projected_oos_actual.py

--- projected_oos_actual.py
+++ projected_oos_actual.py
@@ -106,6 +106,9 @@ if supply_file and oos_file:
             supply = pd.Series({"KOS": 100000, "STL": custom_stl_supply})
         else:
             supply = supply.squeeze()
+
+        if np.isnan(projected_oos):
+            projected_oos = 0.0
         
         oos_data.append({
             "Date": date.strftime("%d %b %Y"),
