29e07b4d72f54c8329e78bbed5d6f13fda71d24f
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-25 12:43:39 +0700

    Update projected_oos_actual.py

--- projected_oos_actual.py
+++ projected_oos_actual.py
@@ -111,7 +111,7 @@ if supply_file and oos_file:
             "Date": date.strftime("%d %b %Y"),
             "KOS Supply": supply.get("KOS", 100000),
             "STL Supply": supply.get("STL", custom_stl_supply),
-            "Projected OOS%": float(projected_oos) if not np.isnan(projected_oos) else 0.0,
+            "Projected OOS%": float(projected_oos),
         })
 
         
