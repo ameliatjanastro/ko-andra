b3b2c61abc8ca273a12a0a2fd67665a69f0ca111
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-26 15:37:01 +0700

    Update projected_oos_actual.py

--- projected_oos_actual.py
+++ projected_oos_actual.py
@@ -118,7 +118,7 @@ if supply_file and oos_file:
             # Zero outbound adjustments
             if date_str in fixed_kos_zero_outbound_days:
                 kos_stock = 0
-                projected_oos += 0.025 * ((1 + supply_factor)*0.9)
+                projected_oos += 0.025 * ((1 + supply_factor)*0.95)
             
             elif date_str in fixed_stl_zero_outbound_days:
                 stl_stock = 0
