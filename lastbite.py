4fd9223c714a687f60444e41a6090df741e3b3fc
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-06-01 17:01:07 +0700

    Update lastbite.py

--- lastbite.py
+++ lastbite.py
@@ -206,6 +206,7 @@ if not modified_result.empty:
     )['forecast_daily'].transform(lambda x: [' (if there\'s campaign)' if v == x.max() and len(x) > 1 else '' for v in x])
 
     for _, row in modified_result.iterrows():
+        label = row['forecast_label'] 
         st.markdown(
             f'<div class="small-font"><h4>🧾 </b> (Product ID: {row["product id"]})</h4></div>',
             unsafe_allow_html=True
