a7c8186903a75594e09f3dbd5b3b4b6c62b92cc7
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-06-01 16:08:10 +0700

    Update lastbite.py

--- lastbite.py
+++ lastbite.py
@@ -104,6 +104,7 @@ try:
     df = soh_df.merge(fc_df[['product id', 'forecast daily']], on='product id').merge(holding_df[['product id', 'holding_cost']], on='product id')
     st.write(df.columns)
     df.drop_duplicates(inplace=True)
+    st.dataframe(df.head())
 
 except KeyError as e:
     st.error(f"❌ Merge failed: {e}")
