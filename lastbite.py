12bf2490fd6e793635edc071998417e0ee1eddd9
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-06-03 21:41:56 +0700

    Update lastbite.py

--- lastbite.py
+++ lastbite.py
@@ -138,9 +138,9 @@ else:
         df.loc[df['brand company'] == selected_brand, 'extra_qty'] = extra_qty_input
 
 # --- Recalculate ---
-df['total_soh'] = df.groupby(['product id','brand company'])['soh'].transform('sum')
+df['total_soh'] = df.groupby('brand company')['soh'].transform('sum')
 df['cogs_ratio'] = df['soh'] / df['total_soh']
-df['extra_qty_allocated'] = df['extra_qty'] * df['cogs_ratio']
+df['extra_qty_allocated'] = extra_qty_input * df['cogs_ratio']
 df['extra_qty_value'] = df['extra_qty_allocated'] * df['cogs']
 df['extra_qty_value_formatted'] = df['extra_qty_value'].apply(lambda x: f"{x:,.0f}" if pd.notnull(x) else "")
 df['doi_current'] = df['soh'] / df['forecast_daily']
