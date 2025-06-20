9efac45d97cd8740f9933ecc35e52b9368f88496
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-06-20 17:37:49 +0700

    Update lastbite.py

--- lastbite.py
+++ lastbite.py
@@ -75,7 +75,7 @@ try:
     def adjust_forecast(row):
         if row['location id'] in [40, 772]:
             if row['location id'] == 772:
-                return row['forecast_daily'] * 0.4
+                return row['forecast_daily'] * 1
             elif row['location id'] == 40:
                 return row['forecast_daily'] * 0.6
         elif row['location id'] in [160, 796]:
