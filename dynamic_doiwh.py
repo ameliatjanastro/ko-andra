ebc099b63099282063da4c86887587d9bcfc48f4
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-06-29 17:18:21 +0700

    Update dynamic_doiwh.py

--- dynamic_doiwh.py
+++ dynamic_doiwh.py
@@ -97,15 +97,19 @@ pareto_weight = {"X": 0, "A": 0, "B": 0, "C": 0}
 product_type_scaler = {"Fresh": 1.0, "Frozen": 1.0, "Dry": 1.0}
 
 with st.expander("⚙️ Adjust Model Parameters", expanded=False):
-
-    col1, col2, col3, col4 = st.columns(4)
+    st.markdown(
+    "**Z-Score Logic (Service Level):**\n"
+    "- Pareto **X** and **A** → **95% service level** (Z = 1.65)\n"
+    "- Pareto **B** and below → **90% service level** (Z = 1.5)"
+    )
+    col1, col2, col3 = st.columns(3)
+    #with col1: 
+        #Z = st.number_input("**Z-Score**", value=1.65, step=0.05, label_visibility="visible")
     with col1:
-        Z = st.number_input("**Z-Score**", value=1.65, step=0.05, label_visibility="visible")
-    with col2:
         ks = st.number_input("**ks (Demand Var)**", value=0.5, step=0.1, label_visibility="visible") if include_safety else 0
-    with col3:
+    with col2:
         kr = st.number_input("**kr (Resched)**", value=0.5, step=0.1, label_visibility="visible") if include_reschedule else 0
-    with col4:
+    with col3:
         kp = st.number_input("**kp (Pareto)**", value=0.5, step=0.1, label_visibility="visible") if include_pareto else 0
 
     if include_pareto:
@@ -169,6 +173,7 @@ def compute_doi(row):
         if not apply_logic:
             final_doi = base_doi
         else:
+            Z = 1.65 if cleaned_pareto in ["X", "A"] else 1.5
             std_d_ratio = row["std_demand"] / row["avg_demand"] if row["avg_demand"] != 0 else 0
 
             safety = (
