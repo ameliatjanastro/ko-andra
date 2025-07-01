0b46da79e6e84728e47011a150cf2e02ebb2e615
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-07-01 17:02:26 +0700

    Update dynamic_doiwh.py

--- dynamic_doiwh.py
+++ dynamic_doiwh.py
@@ -102,14 +102,14 @@ with st.expander("⚙️ Adjust Model Parameters", expanded=False):
     "- Pareto **X** and **A** → **95% service level** (Z = 1.65)\n"
     "- Pareto **B** and below → **90% service level** (Z = 1.5)"
     )
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4 = st.columns(3)
     with col1: 
         Z = st.number_input("**Z-Score**", value=1.65, step=0.05, label_visibility="visible")
-    with col1:
-        ks = st.number_input("**ks (Demand Var)**", value=0.5, step=0.1, label_visibility="visible") if include_safety else 0
     with col2:
-        kr = st.number_input("**kr (Resched)**", value=0.5, step=0.1, label_visibility="visible") if include_reschedule else 0
+        ks = st.number_input("**ks (Demand Var)**", value=0.5, step=0.1, label_visibility="visible") if include_safety else 0
     with col3:
+        kr = st.number_input("**kr (Resched)**", value=0.5, step=0.1, label_visibility="visible") if include_reschedule else 0
+    with col4:
         kp = st.number_input("**kp (Pareto)**", value=0.5, step=0.1, label_visibility="visible") if include_pareto else 0
 
     if include_pareto:
